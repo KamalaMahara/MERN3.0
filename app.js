d5f0faa030d7661ecd5c7c1411be60c0ab884d77
kamala mahara <kmlamahara@gmail.com>
2025-11-26 17:51:57 +0545

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -14,10 +14,24 @@ app.get("/", (request, response) => {
 
 app.post("/blog", upload.single("image"), async (req, res) => {
   const { title, description, subtitle, image } = req.body;
+  const filename = req.file.filename;
+  if (!title || !description || !subtitle || !image) {
+    return res.status(400).json({
+      message: "please provide all the required fields",
+    });
+  }
 
-  await Blog.create({ title, description, subtitle, image });
+  await Blog.create({ title, description, subtitle, image: filename });
+
   res.status(200).json({
     message: "blog api is workingggggggg...",
+  });
+});
+app.get("/blog", async (req, res) => {
+  const blogs = await Blog.find();
+  res.status(200).json({
+    message: "all blogs fetched successfully",
+    data: blogs,
   });
 });
 app.listen(process.env.PORT, () => console.log("port 3000 is running ...."));
